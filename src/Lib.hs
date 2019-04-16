9c78d520eb7bac4ee6674733dd5d09ce28065ba7
liam <liam@magicseaweed.com>
2019-04-16 20:29:51 +0100

    Haddocks for Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,9 +11,13 @@ import Text.Read (readMaybe)
 epoch :: UTCTime
 epoch = UTCTime (fromGregorian 1970 0 0) 0
 
+-- | Convert an integral Unix timestamp (seconds from the epoch) to a 'UTCTime'
 utcTimeFromUnix :: Integral a => a -> UTCTime
 utcTimeFromUnix = flip addUTCTime epoch . fromIntegral
 
+-- | Parse a string value as an Integer representing a Unix timestamp, and
+-- convert to a 'Either' 'String' 'UTCTime`. A 'Left' value indicates a parse
+-- error.
 utcTimeFromStringEither :: String -> Either String UTCTime
 utcTimeFromStringEither s = utcTimeFromUnix <$> tryRead s
   where tryRead (readMaybe' -> Just t) = Right t
